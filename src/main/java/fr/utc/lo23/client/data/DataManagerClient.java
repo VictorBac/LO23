dfa4c9c8085c9f21946a3bba83e55979f0909dc8
Mar60 <hammi.marouane@gmail.com>
2015-11-01 14:53:51 +0100

    [data] DataManagerClient new attribut

--- src/main/java/fr/utc/lo23/client/data/DataManagerClient.java
+++ src/main/java/fr/utc/lo23/client/data/DataManagerClient.java
@@ -1,10 +1,17 @@
 package fr.utc.lo23.client.data;
 
+import fr.utc.lo23.common.data.TableList;
+import fr.utc.lo23.common.data.User;
+import fr.utc.lo23.common.data.UserLightList;
+
 /**
  * Created by Mar on 01/11/2015.
  */
 public class DataManagerClient {
     private InterfaceFromCom interfaceFromCom;
     private InterfaceFromIHMTable interfaceFromIHMTable;
-
+    private User userLocal;
+    private UserLightList listUsers;
+    private TableList listTables;
+    
 }

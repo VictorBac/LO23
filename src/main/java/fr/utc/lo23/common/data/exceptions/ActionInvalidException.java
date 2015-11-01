6f861197db3bc1a5e9ed4f70919038cfc036898a
Mar60 <hammi.marouane@gmail.com>
2015-11-01 14:38:40 +0100

    [data] Action constructor handle exceptions

--- src/main/java/fr/utc/lo23/common/data/exceptions/ActionInvalidException.java
+++ src/main/java/fr/utc/lo23/common/data/exceptions/ActionInvalidException.java
@@ -4,4 +4,7 @@ package fr.utc.lo23.common.data.exceptions;
  * Created by Mar on 01/11/2015.
  */
 public class ActionInvalidException extends Exception {
+    public ActionInvalidException(String message){
+        super(message);
+    }
 }

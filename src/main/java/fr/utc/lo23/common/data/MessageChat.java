c1aaccbe23121eea5cbd65170349a9ba2cbda510
yingdaidy <bailengdy@msn.cn>
2015-11-02 14:38:06 +0100

    [data]Update MessageChat

--- src/main/java/fr/utc/lo23/common/data/MessageChat.java
+++ src/main/java/fr/utc/lo23/common/data/MessageChat.java
@@ -7,6 +7,7 @@ import java.sql.Timestamp;
  * Created by Haroldcb on 21/10/2015.
  */
 public class MessageChat {
+
     /**
      * sender : sender of the message
      * time : moment of the message, used for the coordination
@@ -15,4 +16,34 @@ public class MessageChat {
     private UserLight sender;
     private Timestamp time;
     private String text;
+
+    /**
+     * Constructor
+     * @param sender
+     * @param time
+     * @param text
+     */
+    public MessageChat(UserLight sender, Timestamp time, String text) {
+        this.sender = sender;
+        this.time = time;
+        this.text = text;
+    }
+
+    //getters
+
+    public UserLight getSender() { return sender; }
+
+    public Timestamp getTime() { return time; }
+
+    public String getText() { return text; }
+
+    //setters
+
+    public void setSender(UserLight sender) { this.sender = sender; }
+
+    public void setTime(Timestamp time) { this.time = time; }
+
+    public void setText(String text) { this.text = text; }
+
+
 }

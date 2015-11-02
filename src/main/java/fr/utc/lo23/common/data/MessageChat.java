21c33e45fc96cc58ca1d2ce7fff78db5cf5e0349
yingdaidy <bailengdy@msn.cn>
2015-11-02 13:22:08 +0100

    [data]modify class MessageChat

--- src/main/java/fr/utc/lo23/common/data/MessageChat.java
+++ src/main/java/fr/utc/lo23/common/data/MessageChat.java
@@ -3,14 +3,14 @@ package fr.utc.lo23.common.data;
 import java.sql.Timestamp;
 
 /**
- * Classe représentant un message du chat
+ * Class to represent a message of a chat
  * Created by Haroldcb on 21/10/2015.
  */
 public class MessageChat {
     /**
-     * sender : expéditeur du message
-     * time : moment de l'envoi du message, utile pour la coordination
-     * text : contenu du message
+     * sender : sender of the message
+     * time : moment of the message, used for the coordination
+     * text : content of the message
      */
     private UserLight sender;
     private Timestamp time;

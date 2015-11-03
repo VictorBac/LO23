220c5585fa68481476a9d4cbabf8ec476a7e5149
Jean-Baptiste Martin <git@jb-martin.fr>
2015-11-03 19:30:34 +0100

    Suppression du rouge :)

--- src/main/java/fr/utc/lo23/client/ihm_main/interfaces/InterfaceData.java
+++ src/main/java/fr/utc/lo23/client/ihm_main/interfaces/InterfaceData.java
@@ -1,5 +1,8 @@
 package lo23poker.IHMMain.interfaces;
 
+import fr.utc.lo23.common.data.Stats;
+import fr.utc.lo23.common.data.UserLight;
+
 import java.util.List;
 
 /**
@@ -9,7 +12,7 @@ public interface InterfaceData {
     void remoteUserConnected(UserLight remoteUser);
     void remoteUserProfile(UserLight remoteUser, Stats st);
     void remoteUserDisconnected(UserLight remoteUser);
-    void contactNotificationEvent(userLight remoteUser);
+    void contactNotificationEvent(UserLight remoteUser);
     void onlineUsers(List<UserLight> userList);
     void updateView();
 }

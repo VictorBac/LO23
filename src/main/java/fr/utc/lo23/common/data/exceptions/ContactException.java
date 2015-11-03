051dea829f02cfacd5ed402213e13f06d5dbba9a
Jianghan LI <helloworld11010111@gmail.com>
2015-11-03 16:26:37 +0100

    [data] Create contact exception

---
+++ src/main/java/fr/utc/lo23/common/data/exceptions/ContactException.java
@@ -0,0 +1,10 @@
+package fr.utc.lo23.common.data.exceptions;
+
+/**
+ * Created by Jianghan on 01/11/2015.
+ */
+public class ContactException extends Exception {
+    public ContactException(String message){
+        super(message);
+    }
+}

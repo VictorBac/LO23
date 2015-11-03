90e33c447cf82b6f9fa38f7abad8e99069e1bd56
haroldcb <haroldcb@hotmail.fr>
2015-11-03 19:10:16 +0100

    ajout fichiers PotTest et TableException

---
+++ src/main/java/fr/utc/lo23/common/data/exceptions/TableException.java
@@ -0,0 +1,10 @@
+package fr.utc.lo23.common.data.exceptions;
+
+/**
+ * Created by Haroldcb on 03/11/2015.
+ */
+public class TableException extends Exception{
+    public TableException(String message){
+        System.out.println(message);
+    }
+}

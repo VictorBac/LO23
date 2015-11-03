2bf4feccaee7f52993a9c143bafa50b14c425d2c
Jianghan LI <helloworld11010111@gmail.com>
2015-11-03 16:10:58 +0100

    [data] Add interface for IHM Main

--- src/main/java/fr/utc/lo23/client/data/InterfaceDataFromIHMMain.java
+++ src/main/java/fr/utc/lo23/client/data/InterfaceDataFromIHMMain.java
@@ -3,18 +3,69 @@ package fr.utc.lo23.client.data;
 import fr.utc.lo23.common.data.*;
 
 /**
- * Created by Jianghan on 15-10-20.
+ * This is the interface which will be used by the IHM Main module on the client's side
+ * Created by Jianghan on 20-10-15.
  */
 public interface InterfaceDataFromIHMMain {
-    void logUser(String login,String password);
+    /**
+     * Methode to log in
+     * @param login
+     * @param password
+     */
+    void logUser(String login, String password);
+
+    /**
+     * Methode to ask to exit
+     */
     void exitAsked();
+
+    /**
+     * Methode to save new profile
+     * @param userLocal
+     */
     void saveNewProfile(User userLocal);
+
+    /**
+     * Methode to join a table with mode
+     * @param table
+     * @param mode
+     */
     void joinTableWithMode(Table table, String mode);
-    void tableJoinAcccepted (Table table, String mode);
+
+    /**
+     * Methode to pass on when accepted to join a table
+     * @param table
+     * @param mode
+     */
+    void tableJoinAcccepted(Table table, String mode);
+
+    /**
+     * Methode to get UserList online
+     * @return User Light List
+     */
     UserLightList getPlayerList();
+
+    /**
+     * Methode to get TableList online
+     * @return Table List
+     */
     TableList getTableList();
-    void getListeSavedGame();
+
+    /**
+     * Methode to get saved game list
+     * @return
+     */
     TableList getSavedGamesList();
+
+    /**
+     * Methode to play game on the table
+     * @param idTable
+     */
     void playGame(int idTable);
+
+    /**
+     * Methode to get the user light information
+     * @param userlight
+     */
     void getUser(UserLight userlight);
 }

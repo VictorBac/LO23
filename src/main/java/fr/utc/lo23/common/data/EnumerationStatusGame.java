a16927f156deb03226db9ca190cd0c69669da5a6
yingdaidy <bailengdy@msn.cn>
2015-11-03 19:08:15 +0100

    [data]

--- src/main/java/fr/utc/lo23/common/data/EnumerationStatusGame.java
+++ src/main/java/fr/utc/lo23/common/data/EnumerationStatusGame.java
@@ -4,5 +4,5 @@ package fr.utc.lo23.common.data;
  * Created by Ying on 21/10/2015.
  */
 public enum EnumerationStatusGame {
-    waitingForPlayer,playing,finished
+    waitingForPlayer, playing, finished
 }

2a5eca729d3d7d61d436ab586a87b1cbe40f9e71
Jianghan LI <helloworld11010111@gmail.com>
2015-11-03 16:10:58 +0100

    [data] Add interface for IHM Main

---
+++ src/main/java/fr/utc/lo23/client/data/InterfaceFromIHMMain.java
@@ -0,0 +1,51 @@
+package fr.utc.lo23.client.data;
+
+import fr.utc.lo23.common.data.*;
+
+
+/**
+ * Created by Jianghan on 01-11-15.
+ */
+public class InterfaceFromIHMMain implements InterfaceDataFromIHMMain{
+    public void logUser(String login, String password) {
+
+    }
+
+    public void exitAsked() {
+
+    }
+
+
+    public void saveNewProfile(User userLocal) {
+
+    }
+
+    public void joinTableWithMode(Table table, String mode) {
+
+    }
+
+    public void tableJoinAcccepted(Table table, String mode) {
+
+    }
+
+    public UserLightList getPlayerList() {
+
+    }
+
+    public TableList getTableList() {
+
+    }
+
+    public TableList getSavedGamesList() {
+
+    }
+
+    public void playGame(int idTable) {
+
+    }
+
+    public void getUser(UserLight userlight) {
+
+    }
+
+}

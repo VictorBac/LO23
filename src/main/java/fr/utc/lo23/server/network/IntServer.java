7ebc8463c9bbe5d302954d29dd203a20e7de5af7
Victor Baconneau <victor.baconneau@gmail.com>
2015-11-04 22:17:07 +0100

    [Com] fix interfaces pour pouvoir compiler..

--- src/main/java/fr/utc/lo23/server/network/IntServer.java
+++ src/main/java/fr/utc/lo23/server/network/IntServer.java
@@ -5,10 +5,40 @@
  */
 package fr.utc.lo23.server.network;
 
+import fr.utc.lo23.common.data.Action;
+import fr.utc.lo23.common.data.Table;
+import fr.utc.lo23.common.data.UserLight;
+import fr.utc.lo23.exceptions.network.NetworkFailureException;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author Jean-Côme
  */
 public class IntServer implements InterfaceServer {
-    
+
+    public void sendTableList(ArrayList<Table> tableList) throws NetworkFailureException {
+
+    }
+
+    public void sendPlayers(ArrayList<UserLight> userList) throws NetworkFailureException {
+
+    }
+
+    public void notifyNewPlayer(UserLight userDistant) throws NetworkFailureException {
+
+    }
+
+    public void notifyAction(Action act) throws NetworkFailureException {
+
+    }
+
+    public void notifyNewTable(Table newTable) throws NetworkFailureException {
+
+    }
+
+    public void notifyDisconnection(UserLight distantUser) throws NetworkFailureException {
+
+    }
 }

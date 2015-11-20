ede37abe3a01b301c7151391b0add720a6dea753
Jianghan LI <helloworld11010111@gmail.com>
2015-11-20 15:11:51 +0100

    [data] Finish the first data test.

--- src/tests/fr/utc/lo23/common/data/SerializationTest.java
+++ src/tests/fr/utc/lo23/common/data/SerializationTest.java
@@ -14,12 +14,17 @@ public class SerializationTest {
 
     @Test
     public void testSerializationObject() throws Exception {
+        // Create an localUser with username and password
         User localUser = new User("testLocalUser", "testpsw");
+        // Write localUser into the local data file
         Serialization.serializationObject(localUser, Serialization.pathUserLocal);
+        // Read the local data file into localUser2
         User localUser2 = (User) Serialization.deserializationObject(Serialization.pathUserLocal);
+        // Get the username of localUser and localUser2
         String inputPseudo = localUser.getUserLight().getPseudo();
-        String outputPseudo = localUser.getUserLight().getPseudo();
-        assertEquals("The comparasion between serialization input and output", inputPseudo, outputPseudo);
+        String outputPseudo = localUser2.getUserLight().getPseudo();
+        // Compare the usernames
+        assertEquals("The comparison between serialization input and output", inputPseudo, outputPseudo);
     }
 
     @Test

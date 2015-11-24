8f0103435c0399d4b6f9b746a34b1faa7a4faf02
Guillaume Vassal <gp.vassal@gmail.com>
2015-11-24 19:17:02 +0100

    [Com] Menage ServerLink

---
+++ src/main/java/fr/utc/lo23/common/Params.java
@@ -0,0 +1,9 @@
+package fr.utc.lo23.common;
+
+/**
+ * Created by guixii on 24/11/15.
+ */
+public class Params {
+    public static final String DEFAULT_SERVER_ADDRESS = "localhost";
+    public static final int DEFAULT_SERVER_PORT = 1902;
+}

06b4779be7df408b2e10210fe9ccfefa054b7b49
Zangdar1111 <brochetonthibault@gmail.com>
2015-11-24 14:21:29 +0100

    [IHM-Table] Amélioration visuelle (actions, table), première étape

--- src/main/java/fr/utc/lo23/client/ihm_table/ITableToMainListener.java
+++ src/main/java/fr/utc/lo23/client/ihm_table/ITableToMainListener.java
@@ -6,14 +6,14 @@ import javafx.scene.layout.Pane;
 public interface ITableToMainListener {
 
 	/*
-	 * Fonction à appeler lorsque l'utilisateur veut créer une table
-	 * Permet à IHM-Table de prendre la main et d'afficher le formulaire de création de table
+	 * Fonction Ã  appeler lorsque l'utilisateur veut crÃ©er une table
+	 * Permet Ã  IHM-Table de prendre la main et d'afficher le formulaire de crÃ©ation de table
 	 */
 	public void showTableCreationForm(Pane root);
 
     /*
-     * Fonction à appeler lorsque l'utilisateur veut rejoindre une table
-     * Permet à IHM-Table de prendre la main et d'afficher la table
+     * Fonction Ã  appeler lorsque l'utilisateur veut rejoindre une table
+     * Permet Ã  IHM-Table de prendre la main et d'afficher la table
      */
     public void joinTable(Pane root, Table table);
 }

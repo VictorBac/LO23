aa7fb99af5e69e3e9dc445af251791c7180a079b
fred54440 <fred54440@hotmail.fr>
2015-11-24 19:02:04 +0100

    integration de 2 fenetres

--- src/main/java/fr/utc/lo23/client/ihm_main/controllers/ConnectionController.java
+++ src/main/java/fr/utc/lo23/client/ihm_main/controllers/ConnectionController.java
@@ -6,12 +6,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,9 +45,19 @@ public class ConnectionController implements Initializable {
     public ListView listViewServers;
 
     @FXML
-    void didButtonConnectClick(ActionEvent event) {
+    void didButtonConnectClick(ActionEvent event) throws IOException {
         System.out.println("didButtonConnectClick");
 
+        Node node=(Node) event.getSource();
+        Stage stage=(Stage) node.getScene().getWindow();
+        URL tmp = getClass().getResource("/fr/utc/lo23/client/ihm_main/ui/principal.fxml");
+        System.out.println(tmp.toString());
+        Parent root = FXMLLoader.load(tmp);/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("/fr/utc/lo23/client/ihm_main/ui/style.css").toExternalForm());
+        root.setStyle("-fx-background-image: url('/fr/utc/lo23/client/ihm_main/ui/poker.png')");
+        stage.show();
         String log = fieldUsername.getText();
 
         log = fieldUsername.getText();

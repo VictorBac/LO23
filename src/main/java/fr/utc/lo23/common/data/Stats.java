00f1724178d0b40be08763099a50909f235129e3
Jianghan LI <helloworld11010111@gmail.com>
2015-11-24 19:33:00 +0100

    [data] Code in English.

--- src/main/java/fr/utc/lo23/common/data/Stats.java
+++ src/main/java/fr/utc/lo23/common/data/Stats.java
@@ -3,7 +3,7 @@ package fr.utc.lo23.common.data;
 import java.util.ArrayList;
 
 /**
- * Created by Rémy on 20/10/2015.
+ * Created by Jianghan on 20/10/2015.
  */
 public class Stats {
 
@@ -11,28 +11,28 @@ public class Stats {
     private ArrayList<Integer> listPointsByGame;
 
     /**
-     * permet de mettre à jour les deux ArrayLists
-     * @param beginMse : mise de départ du joueur
+     * Method to update these two ArrayLists
+     * @param beginMse : mise de depart du joueur
      * @param points : points par partie du joueur
      */
     public void updateStats(int beginMse, int points){
         listMiseBegin.add(beginMse);
         listPointsByGame.add(points);
-    };
+    }
 
     /**
      * Ajoute une nouvelle stat au joueur
-     * @param newStat ?
-     * TODO : préciser usage fonction
+     * @param newStat
+     * TODO : how does it work?
      */
     public void addNewStat(int newStat){
 
 
-    };
+    }
 
     /**
-     * getter de listPointsByGame
-     * @return l'attribut listPointsByGame
+     * getter of listPointsByGame
+     * @return the attribute listPointsByGame
      */
     public ArrayList<Integer> getStats(){
         return listPointsByGame;

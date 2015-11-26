f660053b941ae67968bfb33fd339e98524671a38
Jianghan LI <helloworld11010111@gmail.com>
2015-11-26 22:10:37 +0100

    [data] Add first test to CombinationCalculatorTest

--- src/tests/fr/utc/lo23/server/data/CombinationCalculatorTest.java
+++ src/tests/fr/utc/lo23/server/data/CombinationCalculatorTest.java
@@ -1,20 +1,31 @@
 package fr.utc.lo23.server.data;
 
-import fr.utc.lo23.common.data.Pot;
+import fr.utc.lo23.common.data.Card;
 import org.junit.Test;
-
+import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Jianghan on 26/11/2015.
  */
 public class CombinationCalculatorTest {
+    public CombinationCalculator calculator = new CombinationCalculator();
+    ArrayList<Integer> cardValues;
 
+    public CombinationCalculatorTest() {
+        cardValues = new ArrayList<Integer>();
+        cardValues.add(13);
+        cardValues.add(10);
+        cardValues.add(10);
+        cardValues.add(9);
+        cardValues.add(8);
+        cardValues.add(8);
+        cardValues.add(8);
+    }
 
     @Test
     public void testHighCard() throws Exception {
-
-        System.out.println ("hi");
-        //assertEquals(resOptimal,res);
+        int result = calculator.hasHighCard(cardValues);
+        assertEquals(result, 13);
     }
 }

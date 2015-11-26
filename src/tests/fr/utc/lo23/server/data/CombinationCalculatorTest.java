e4cf0bf7e376f0109659b70753422c9c226a81f0
Jianghan LI <helloworld11010111@gmail.com>
2015-11-26 21:05:39 +0100

    [data] Add Combination calculator test.

---
+++ src/tests/fr/utc/lo23/server/data/CombinationCalculatorTest.java
@@ -0,0 +1,20 @@
+package fr.utc.lo23.server.data;
+
+import fr.utc.lo23.common.data.Pot;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jianghan on 26/11/2015.
+ */
+public class CombinationCalculatorTest {
+
+
+    @Test
+    public void testHighCard() throws Exception {
+
+        System.out.println ("hi");
+        //assertEquals(resOptimal,res);
+    }
+}

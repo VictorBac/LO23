266ddc30d678f25832d07268166b1b38ca0c3c13
Jianghan LI <helloworld11010111@gmail.com>
2015-11-26 21:43:37 +0100

    [data] Code in English

--- src/main/java/fr/utc/lo23/server/data/CombinationCalculator.java
+++ src/main/java/fr/utc/lo23/server/data/CombinationCalculator.java
@@ -1,19 +1,18 @@
 package fr.utc.lo23.server.data;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import fr.utc.lo23.common.data.Card;
 import fr.utc.lo23.common.data.PlayerHand;
 
 import java.util.ArrayList;
 
 /**
- * Classe de calcul des combinaisons determiannt le gagnant d'un tour
- * Created by Haroldcb on 21/10/2015.
+ * Class of combination calculator to find the winner
+ * Created by Jianghan on 21/10/2015.
  */
 public class CombinationCalculator {
     /**
-     * @param listOfPlayer : liste des joueurs de la table et de leurs cartes,
-     * @param cardsOnField : Cartes sur la table
+     * @param listOfPlayer : player list and their hand cards
+     * @param cardsOnField : cards on the table
      */
     public ArrayList<PlayerHand> getWinner(ArrayList<PlayerHand> listOfPlayer, ArrayList<Card> cardsOnField){
 
@@ -27,8 +26,12 @@ public class CombinationCalculator {
         if(cards.size() != 7) {
             throw new Exception();
         }
-        
+
 
         return null; //TODO remove this line
+    }
+
+    public Integer hasHighCard(ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
     }
 }

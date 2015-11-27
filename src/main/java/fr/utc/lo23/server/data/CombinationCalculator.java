8931a2ac5817f1e2d92e7870dc26500b50708553
Jianghan LI <helloworld11010111@gmail.com>
2015-11-27 11:13:35 +0100

    [data] Enhancement for combination calculator

--- src/main/java/fr/utc/lo23/server/data/CombinationCalculator.java
+++ src/main/java/fr/utc/lo23/server/data/CombinationCalculator.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
  * Created by Jianghan on 21/10/2015.
  */
 public class CombinationCalculator {
+
     /**
      * @param listOfPlayer : player list and their hand cards
      * @param cardsOnField : cards on the table
@@ -31,7 +32,63 @@ public class CombinationCalculator {
         return null; //TODO remove this line
     }
 
+    public ArrayList<Integer> toMap(ArrayList<Integer> cardValues) throws Exception {
+        for (int i = 0; i < 7 ; i++) {
+
+        }
+        return cardValues;
+    }
+
+
     public Integer hasHighCard(ArrayList<Integer> cardValues) throws Exception {
         return cardValues.get(0);
     }
+
+    public Integer hasOnePair(ArrayList<Integer> cardValues) throws Exception {
+        for (int i = 0; i < cardValues.size() - 1; i++) {
+            if (cardValues.get(i) == cardValues.get(i+1)) {
+                return cardValues.get(i);
+            }
+        }
+        return 0;
+    }
+
+    public Integer hasTwoPair(ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
+    }
+
+    public Integer hasThree(ArrayList<Integer> cardValues) throws Exception {
+        for (int i = 0; i < cardValues.size() - 2; i++) {
+            if (cardValues.get(i) == cardValues.get(i+2)) {
+                return cardValues.get(i);
+            }
+        }
+        return 0;
+    }
+
+    public Integer hasStraight(ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
+    }
+
+    public Integer hasFlush (ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
+    }
+
+    public Integer hasFour(ArrayList<Integer> cardValues) throws Exception {
+        for (int i = 0; i < cardValues.size() - 3; i++) {
+            if (cardValues.get(i) == cardValues.get(i+3)) {
+                return cardValues.get(i);
+            }
+        }
+        return 0;
+    }
+
+    public Integer hasStraightFlush (ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
+    }
+
+    public Integer hasRoyalFlush(ArrayList<Integer> cardValues) throws Exception {
+        return cardValues.get(0);
+    }
+
 }
--- src/tests/fr/utc/lo23/server/data/CombinationCalculatorTest.java
+++ src/tests/fr/utc/lo23/server/data/CombinationCalculatorTest.java
@@ -3,29 +3,58 @@ package fr.utc.lo23.server.data;
 import fr.utc.lo23.common.data.Card;
 import org.junit.Test;
 import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Jianghan on 26/11/2015.
  */
 public class CombinationCalculatorTest {
-    public CombinationCalculator calculator = new CombinationCalculator();
+    public CombinationCalculator calculator;
     ArrayList<Integer> cardValues;
+    ArrayList<Integer> map;
 
     public CombinationCalculatorTest() {
+        calculator = new CombinationCalculator();
         cardValues = new ArrayList<Integer>();
         cardValues.add(13);
         cardValues.add(10);
         cardValues.add(10);
         cardValues.add(9);
-        cardValues.add(8);
-        cardValues.add(8);
-        cardValues.add(8);
+        cardValues.add(9);
+        cardValues.add(9);
+        cardValues.add(3);
     }
 
     @Test
     public void testHighCard() throws Exception {
         int result = calculator.hasHighCard(cardValues);
         assertEquals(result, 13);
+    }
+
+    @Test
+    public void testOnePair() throws Exception {
+        int result = calculator.hasOnePair(cardValues);
+        assertEquals(result, 10);
+    }
+
+    @Test
+    public void testThree() throws Exception {
+        int result = calculator.hasThree(cardValues);
+        assertEquals(result, 9);
+    }
+
+    @Test
+    public void testFour() throws Exception {
+        int result = calculator.hasFour(cardValues);
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void tempTest() throws Exception {
+        Collections.sort(cardValues);
+        Collections.reverse(cardValues);
+
     }
 }

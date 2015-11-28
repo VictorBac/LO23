23ef833f30f8d2f27c44f40313ef2ae2c0b85fec
Zangdar1111 <brochetonthibault@gmail.com>
2015-11-28 23:54:10 +0100

    [IHM-Table] fin mise à jour graphique + ajouts fonctions gestion actions

--- src/main/java/fr/utc/lo23/client/ihm_table/controllers/TableController.java
+++ src/main/java/fr/utc/lo23/client/ihm_table/controllers/TableController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
@@ -40,13 +41,19 @@ public class TableController {
         chatInitializer();
     }
 
-	public TableController(){
+    public TableController(){
         playerControllerMap = new HashMap<UserLight,PlayerController>();
         defaultImage = new Image(getClass().getResource("../images/default.png").toExternalForm());
-	}
+    }
 
     public void initialize(){
         if(isHost) btnLaunchGame.setVisible(true);
+        showActionBox();
+        enableActionFold();
+        enableActionCheck();
+        enableActionFollow();
+        enableActionBet();
+        enableActionAllin();
     }
 
     public void playerInitializer(){
@@ -79,8 +86,10 @@ public class TableController {
         playerControllerMap.put(user, playerView.createPlayer(tablePane, user, coords, image));
     }
 
-	@FXML
-	private TextField messageToSend;
+
+
+    @FXML
+    private TextField messageToSend;
     @FXML
     private ListView<String> chatList;
     @FXML
@@ -88,15 +97,37 @@ public class TableController {
     @FXML
     private Pane tablePane;
 
+    @FXML
+    private Pane actionBox;
+    @FXML
+    private Button actionFold;
+    @FXML
+    private Button actionCheck;
+    @FXML
+    private Button actionFollow;
+    @FXML
+    private Button actionBet;
+    @FXML
+    private Slider actionBetMoneySelector;
+    @FXML
+    private Button actionAllin;
+
 	@FXML
 	private void sendMessage(javafx.event.ActionEvent event) {
         //TODO : getUserLight
+        // à voir comment on fait ? est-ce qu'on doit le récupérer de data ou de ihm main ? plutot de data, donc il nous faut une fonction
+        //TO DELETE
         UserLight self = new UserLight();
 
         Timestamp time = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
         MessageChat message = new MessageChat(self,time,messageToSend.getText());
 
-        sendPacket(message);
+        //TODO : Raccorder à DATA
+        //ihmTable.getDataInterface(). fucking fontion qui existe pas
+
+        //TO DELETE
+        addChatMessage(message);
+
         messageToSend.clear();
 	}
 
@@ -107,18 +138,79 @@ public class TableController {
         //ihmTable.getDataInterface().playGame(table.getIdTable());
     }
 
-    private void sendPacket(MessageChat message) {
-        //TODO : Raccorder à DATA
-        //ihmTable.getDataInterface(). fucking fontion qui existe pas
-
-        //TO DELETE
-        addChatMessage(message);
-    }
-
     public void addChatMessage(MessageChat message) {
         chatList.getItems().add(message.getSender().getPseudo() + " : " + message.getText());
         chatList.setStyle("-fx-graphic:red;");
     }
+
+    public void showActionBox(){
+        actionBox.setVisible(true);
+    }
+
+    public void hideActionBox(){
+        actionBox.setVisible(false);
+    }
+
+    public void enableActionFold(){
+        actionFold.getStyleClass().remove("action_fold_off");
+        actionFold.getStyleClass().add("active");
+        actionFold.getStyleClass().add("action_fold_on");
+    }
+
+    public void disableActionFold(){
+        actionFold.getStyleClass().remove("action_fold_on");
+        actionFold.getStyleClass().remove("active");
+        actionFold.getStyleClass().add("action_fold_off");
+    }
+
+    public void enableActionCheck(){
+        actionCheck.getStyleClass().remove("action_check_off");
+        actionCheck.getStyleClass().add("active");
+        actionCheck.getStyleClass().add("action_check_on");
+    }
+
+    public void disableActionCheck(){
+        actionCheck.getStyleClass().remove("action_check_on");
+        actionCheck.getStyleClass().remove("active");
+        actionCheck.getStyleClass().add("action_check_off");
+    }
+
+    public void enableActionFollow(){
+        actionFollow.getStyleClass().remove("action_follow_off");
+        actionFollow.getStyleClass().add("active");
+        actionFollow.getStyleClass().add("action_follow_on");
+    }
+
+    public void disableActionFollow(){
+        actionFollow.getStyleClass().remove("action_follow_on");
+        actionFollow.getStyleClass().remove("active");
+        actionFollow.getStyleClass().add("action_follow_off");
+    }
+
+    public void enableActionBet(){
+        actionBet.getStyleClass().remove("action_bet_off");
+        actionBet.getStyleClass().add("active");
+        actionBet.getStyleClass().add("action_bet_on");
+    }
+
+    public void disableAction(){
+        actionBet.getStyleClass().remove("action_bet_on");
+        actionBet.getStyleClass().remove("active");
+        actionBet.getStyleClass().add("action_bet_off");
+    }
+
+    public void enableActionAllin(){
+        actionAllin.getStyleClass().remove("action_allin_off");
+        actionAllin.getStyleClass().add("active");
+        actionAllin.getStyleClass().add("action_allin_on");
+    }
+
+    public void disableActionAllin(){
+        actionAllin.getStyleClass().remove("action_allin_on");
+        actionAllin.getStyleClass().remove("active");
+        actionAllin.getStyleClass().add("action_allin_off");
+    }
+
 
 
 }

c28647380f32a588687ba9b78079ccab06791cad
Raboul <leclerc.vic@gmail.com>
2015-12-01 18:50:56 +0100

    fonction pour create profil

--- src/main/java/fr/utc/lo23/client/ihm_main/controllers/CreateController.java
+++ src/main/java/fr/utc/lo23/client/ihm_main/controllers/CreateController.java
@@ -2,8 +2,10 @@ package fr.utc.lo23.client.ihm_main.controllers;
 
 import fr.utc.lo23.common.data.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 
+import javax.swing.text.html.ImageView;
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,13 +15,35 @@ import java.util.ResourceBundle;
  */
 public class CreateController extends BaseController {
 
-    public TextField username;
-    public TextField firstname;
-    public TextField lastname;
-    public TextField birthdate;
-    public TextField email;
-    public PasswordField password;
-    public PasswordField repassword;
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private Button create;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField firstname;
+
+    @FXML
+    private TextField lastname;
+
+    @FXML
+    private TextField birthdate;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField repassword;
+
+    @FXML
+    private ImageView imageviewer;
 
 
     public void createNewUser(ActionEvent actionEvent) {
@@ -27,8 +51,15 @@ public class CreateController extends BaseController {
         User createUser = new User();
         //TODO quand data aura commit dans le main on pourra recuperer les setters du USER.
 
+        //createUser.setUSERNAME(username.getText());
+        //createUser.setFIRSTNAME(firstname.getText());
+        //createUser.setLASTNAME(lastname.getText());
+        //createUser.setEMAIL(email.getText());
+        // ETC....
+
+
         mController.getManagerMain().getInterfaceDataToMain().saveNewProfile(createUser);
-        
+
 
     }
 

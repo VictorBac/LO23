d522d5872e75914b85f96d83b7e28d09b1535255
jcomedouteau <jcome.douteau@gmail.com>
2015-12-01 17:37:54 +0100

    Modification de la fonction JoinTable

--- src/main/java/fr/utc/lo23/client/network/NetworkManagerClient.java
+++ src/main/java/fr/utc/lo23/client/network/NetworkManagerClient.java
@@ -117,8 +117,9 @@ public class NetworkManagerClient implements InterfaceClient  {
 
     }
 
-    public void joinTable(UserLight userLocal, int IdTable) throws NetworkFailureException, FullTableException {
-
+    public void joinTable(UserLight userLocal, Table tableToJoin) throws NetworkFailureException, FullTableException {
+        Console.log("Tentative de rejoingnement de table");
+        //RequestJoinTableMessage RequestJoinTableMes = new RequestJoinTableMessage(userLocal,IdTable);
     }
 
     /**

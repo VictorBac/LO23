5a47a11b72e3b26637379f144d42de26c682a1b1
RapaelKovacic <raphael.kovacic@gmail.com>
2015-12-01 17:17:44 +0100

    Launch Game Message specification

--- src/main/java/fr/utc/lo23/common/network/LaunchGameMessage.java
+++ src/main/java/fr/utc/lo23/common/network/LaunchGameMessage.java
@@ -1,24 +1,41 @@
 package fr.utc.lo23.common.network;
 
+import fr.utc.lo23.client.network.main.Console;
 import fr.utc.lo23.client.network.threads.ServerLink;
+import fr.utc.lo23.common.data.UserLight;
 import fr.utc.lo23.server.network.threads.ConnectionThread;
+import fr.utc.lo23.server.network.threads.PokerServer;
+
+import java.util.UUID;
 
 /**
  * Created by raphael on 30/11/15.
  */
 public class LaunchGameMessage extends Message {
+    private UUID idTable;
+    private UserLight UserInit;
+    public LaunchGameMessage(UUID idTable,UserLight u) {
+        this.idTable = idTable;
+        this.UserInit = u;
+    }
+
     @Override
     public void process() {
-
     }
 
     @Override
     public void process(ConnectionThread threadServer) {
-
+        PokerServer myServ = threadServer.getMyServer();
+        Console.log("Launch Game message received");
+        myServ.getNetworkManager().getDataInstance().startGame(idTable,UserInit);
     }
 
     @Override
     public void process(ServerLink threadClient) {
+        Console.log("Launch Game message received");
 
+        Console.log("Game is starting");
+
+        //threadClient.getNetworkManager().getDataInstance().
     }
 }

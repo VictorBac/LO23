632829732be51b612c19e4bc0547d70700b6406a
Jianghan LI <helloworld11010111@gmail.com>
2015-12-01 23:59:22 +0100

    [data] Add getWinner function to CombinationCalculator.

--- src/main/java/fr/utc/lo23/server/data/exceptions/CardValueNumberInvalidException.java
+++ src/main/java/fr/utc/lo23/server/data/exceptions/CardValueNumberInvalidException.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 /**
  * Created by Jianghan on 15-12-1.
  */
-public class CardValueNumberInvalideException extends Exception {
-    public CardValueNumberInvalideException(ArrayList[] cardValue) {
+public class CardValueNumberInvalidException extends Exception {
+    public CardValueNumberInvalidException(ArrayList<Integer> cardValue) {
         System.out.println("There is something wrong with the number of card value:" + cardValue);
     }
 }

6ecac032fbde2365cc918f17b496ff568b28ada7
Zangdar1111 <brochetonthibault@gmail.com>
2015-12-03 17:10:31 +0100

    [IHM-Main] Création des managers et envoi des interfaces opérationnel

--- src/main/java/fr/utc/lo23/client/ihm_main/IHMMainClientManager.java
+++ src/main/java/fr/utc/lo23/client/ihm_main/IHMMainClientManager.java
@@ -16,65 +16,120 @@ import fr.utc.lo23.client.network.NetworkManagerClient;
  */
 public class IHMMainClientManager {
 
-    public static InterfaceMainToData getInterfaceMainToData() {
-        return interMainToData;
-    }
-
-    public static InterfaceMainToTable getInterfaceMainToTable() {
-        return interMainToTable;
-    }
-
-    public static InterfaceDataFromIHMMain getInterfaceDataToMain() {
-        return managerData.getInterFromIHMMain();
-    }
-
-    public static ITableToMainListener getInterfaceTableToMain() {
-        return managerTable.getTableToMainListener();
-    }
-
-
-
     /**
-     * Interfaces from MAIN
+     * Nos interfaces
      */
-    private static InterfaceMainToData interMainToData;
-    private static InterfaceMainToTable interMainToTable;
+    private InterfaceMainToData interMainToData;
+    private InterfaceMainToTable interMainToTable;
 
+    /*
+     * Interfacesà récupérer
+     */
+    private ITableToMainListener interTableToMain;
+    private InterfaceDataFromIHMMain interDataToMain;
 
     /**
      * Managers
      */
-    private static DataManagerClient managerData;
-    private static NetworkManagerClient managerNetwork;
-    private static IHMTable managerTable;
+    private DataManagerClient managerData;
+    private NetworkManagerClient managerNetwork;
+    private IHMTable managerTable;
 
 
+    // Attributs privés
 
-
-    private static ConnectionController controllerConnection;
-    private static MainControllerClient controllerMain;
+    private ConnectionController controllerConnection;
+    private MainControllerClient controllerMain;
     // TODO ajouter profileController
 
 
-    public static ConnectionController getControllerConnection() throws NullPointerException {
+    //Getters et setters de nos interfaces
+
+    public InterfaceMainToData getInterMainToData() {
+        return interMainToData;
+    }
+
+    public void setInterMainToData(InterfaceMainToData interMainToData) {
+        this.interMainToData = interMainToData;
+    }
+
+    public InterfaceMainToTable getInterMainToTable() {
+        return interMainToTable;
+    }
+
+    public void setInterMainToTable(InterfaceMainToTable interMainToTable) {
+        this.interMainToTable = interMainToTable;
+    }
+
+
+    //Getters et setters des interfaces à récupérer
+
+    public InterfaceDataFromIHMMain getInterDataToMain() {
+        return interDataToMain;
+    }
+
+    public void setInterDataToMain(InterfaceDataFromIHMMain interDataToMain) {
+        this.interDataToMain = interDataToMain;
+    }
+
+    public ITableToMainListener getInterTableToMain() {
+        return interTableToMain;
+    }
+
+    public void setInterTableToMain(ITableToMainListener interTableToMain) {
+        this.interTableToMain = interTableToMain;
+    }
+
+
+    //Getters et setters des managers
+
+    public IHMTable getManagerTable() {
+        return managerTable;
+    }
+
+    public void setManagerTable(IHMTable managerTable) {
+        this.managerTable = managerTable;
+    }
+
+    public NetworkManagerClient getManagerNetwork() {
+        return managerNetwork;
+    }
+
+    public void setManagerNetwork(NetworkManagerClient managerNetwork) {
+        this.managerNetwork = managerNetwork;
+    }
+
+    public DataManagerClient getManagerData() {
+        return managerData;
+    }
+
+    public void setManagerData(DataManagerClient managerData) {
+        this.managerData = managerData;
+    }
+
+
+    //Getters et setters de nos attributs privés
+
+
+    public ConnectionController getControllerConnection() throws NullPointerException {
         if (controllerConnection == null) {
             throw  new NullPointerException("controllerConnection is NULL");
         }
         return controllerConnection;
     }
 
-    public static MainControllerClient getControllerMain() throws NullPointerException {
+    public MainControllerClient getControllerMain() throws NullPointerException {
         if (controllerMain == null) {
             throw new NullPointerException("controllerMain is NULL");
         }
         return controllerMain;
     }
 
-    public static void setControllerConnection(ConnectionController controllerConnection) {
+    public void setControllerConnection(ConnectionController controllerConnection) {
         controllerConnection = controllerConnection;
     }
 
-    public static void setControllerMain(MainControllerClient controllerMain) {
+    public void setControllerMain(MainControllerClient controllerMain) {
         controllerMain = controllerMain;
     }
 
@@ -84,25 +139,28 @@ public class IHMMainClientManager {
 
     public IHMMainClientManager() {
 
+        //Création des managers
         managerData = new DataManagerClient();
         managerNetwork = new NetworkManagerClient(managerData.getInterFromCom());
         managerTable = new IHMTable();
 
+        //Création de nos interfaces
         interMainToData = new InterData(this);
         interMainToTable = new InterTable(this);
 
-
+        //Link des interfaces de Data
         managerData.setInterToCom(managerNetwork);
         managerData.setInterToIHMTable(managerTable.getTableToDataListener());
         managerData.setInterToIHMMain(interMainToData);
 
-        managerTable.setDataInterface((InterfaceFromIHMTable) managerData.getInterFromIHMTable());
+        //Link des interfaces de Table
+        managerTable.setDataInterface(managerData.getInterFromIHMTable());
+        managerTable.setMainInterface(this.getInterMainToTable());
 
-        // TODO setInterMainToTable
-        
+        //Link de nos interfaces
+        this.setInterMainToData(managerData.getInterToIHMMain());
+        this.setInterMainToTable(managerTable.getMainInterface());
 
-        // TODO
-//        interComToData = new
     }
 
 

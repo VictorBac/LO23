198088d63cc43b89a9f37ce4a2a35aa1c7710606
Jianghan LI <helloworld11010111@gmail.com>
2015-12-21 19:57:21 +0100

    [data] Add importProfileFile et exportProfileFile.

--- src/main/java/fr/utc/lo23/client/data/InterfaceFromIHMMain.java
+++ src/main/java/fr/utc/lo23/client/data/InterfaceFromIHMMain.java
@@ -74,6 +74,7 @@ public class InterfaceFromIHMMain implements InterfaceDataFromIHMMain{
      */
     public void saveNewProfile(User userLocal) {
         String login = userLocal.getLogin();
+        dManagerClient.setUserLocal(userLocal);
         Serialization.serializationObject(userLocal, Serialization.dirLocalSavedFiles + login);
     }
 
@@ -188,6 +189,17 @@ public class InterfaceFromIHMMain implements InterfaceDataFromIHMMain{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void importProfileFile(String filePath) {
+        User profile = (User) Serialization.deserializationObject(filePath);
+        saveNewProfile(profile);
+    }
+
+    @Override
+    public void exportProfileFile(String filePath) {
+        Serialization.serializationObject(dManagerClient.getUserLocal(), filePath);
     }
 
 

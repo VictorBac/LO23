bbe1d9a5b494deaec731f78077dc86a169c27cf2
Jianghan LI <helloworld11010111@gmail.com>
2015-12-22 09:16:59 +0100

    [data] Statis

--- src/main/java/fr/utc/lo23/common/data/Stats.java
+++ src/main/java/fr/utc/lo23/common/data/Stats.java
@@ -2,6 +2,7 @@ package fr.utc.lo23.common.data;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import fr.utc.lo23.common.data.exceptions.StatsSizeException;
 
 /**
  * Class for statistic. Hope it will be used.
@@ -23,20 +24,75 @@ public class Stats implements Serializable {
      * Set startPoints List and endPoints List.
      * @param start start points list
      * @param end end points list
+     * @return 1 if successful
+     * @throws StatsSizeException
      */
-    public void setStats(ArrayList<Integer> start, ArrayList<Integer> end){
-        this.startPointsList = start;
-        this.endPointsList = end;
+    public int setStats(ArrayList<Integer> start, ArrayList<Integer> end) throws StatsSizeException {
+        if (start.size() != end.size()) {
+            throw new StatsSizeException(start.size(), end.size());
+        } else {
+            this.startPointsList = start;
+            this.endPointsList = end;
+            return 1;
+        }
     }
 
     /**
      * Add a new record to the statistic
      * @param startPoint mise de depart du joueur
      * @param endPoints points par partie du joueur
+     * @return 1 if successful
+     * @throws StatsSizeException
      */
-    public void addNewStat(int startPoint, int endPoints){
+    public int addNewStat(Integer startPoint, Integer endPoints) throws StatsSizeException {
+        if (startPoint == null) {
+            throw new StatsSizeException(0, 1);
+        }
+        if (endPoints == null) {
+            throw new StatsSizeException(1, 0);
+        }
         startPointsList.add(startPoint);
         endPointsList.add(endPoints);
+        return 1;
+    }
+
+    public int getGameNumber() {
+        return this.endPointsList.size();
+    }
+
+    public Integer getMeanEndPoints() {
+        Integer total = 0;
+        for (Integer point : endPointsList) {
+            total += point;
+        }
+        if (endPointsList.size() == 0) {
+            return 0;
+        } else {
+            return total/endPointsList.size();
+        }
+    }
+
+    public Integer getMeanStartPoints() {
+        Integer total = 0;
+        for (Integer point : startPointsList) {
+            total += point;
+        }
+        for (Integer point : endPointsList) {
+            total += point;
+        }
+        return total;
+    }
+
+    public Integer getTotalPoints() {
+        Integer total = 0;
+        for (Integer point : startPointsList) {
+            total += point;
+        }
+        if (startPointsList.size() == 0) {
+            return 0;
+        } else {
+            return total/startPointsList.size();
+        }
     }
 
     /******************* Gettor **********************/

32786b790496c0596b2ea7e01808035629fa7075
Jianghan LI <helloworld11010111@gmail.com>
2016-01-02 19:07:27 +0100

    [data] fix.

--- src/tests/fr/utc/lo23/client/data/InterfaceFromIHMMainTest.java
+++ src/tests/fr/utc/lo23/client/data/InterfaceFromIHMMainTest.java
@@ -1,11 +1,9 @@
 package fr.utc.lo23.client.data;
 
-import fr.utc.lo23.common.data.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 /**
+ * test fro interface IHM MAIN
  * Created by Jianghan on 01/11/2015.
  */
 public class InterfaceFromIHMMainTest {
@@ -13,19 +11,7 @@ public class InterfaceFromIHMMainTest {
 
 
     @Test
-    public void testLogUser() throws Exception {
-        // Create an test localUser with username and password
-        User localUser = new User("testLocalUser", "testpsw");
-        // Write test localUser into the local data file
-        Serialization.serializationObject(localUser, Serialization.dirLocalSavedFiles + "testUserLocal");
-        // Call logUser in interfaceDataFromIHMMain
-        //interfaceDataFromIHMMain.logUser("testLocalUser", "testpsw");
-    }
-
-
-    @Test
     public void importFilesTest() {
-//        interfaceDataFromIHMMain.importFiles("/Users/mac/Desktop/src/tmp");
         interfaceDataFromIHMMain.importFiles("./tmp/src");
     }
 }
